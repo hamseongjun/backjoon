59115597bf80d930463ec7e10c01bc21e6acdbbe
hamseongjun <gkatjdwns10@gmail.com>
2023-12-20 20:42:04 +0900

    Multiple & Divisor 1

---
+++ Step by step/Multiple&Divisor/Aone/Main.java
@@ -0,0 +1,29 @@
+package Aone;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = -1;
+        int b = -1;
+
+        while (true) {
+            a = scanner.nextInt();
+            b = scanner.nextInt();
+            if (a == 0 && b == 0) {
+                break;
+            }
+            if (b % a == 0) {
+                System.out.println("factor");
+            }
+            else if (a % b == 0) {
+                System.out.println("multiple");
+            }
+            else {
+                System.out.println("neither");
+            }
+        }
+    }
+}
